39654a9e3697ebaf7b4cd0efbc30739c8f535f32
GM Iqbal Mahmud <36500813+gmiqbal@users.noreply.github.com>
2023-04-07 02:33:07 +0600

    1

---
+++ SnakeGame.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class SnakeGame
+{
+    public static void main(String[] args) {
+//        GameFrame frame = new GameFrame();
+//        if you don't wanna use 'frame' then you can simply write above line in below style:
+        new GameFrame();
+    }
+}
